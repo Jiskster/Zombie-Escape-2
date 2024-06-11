26eac16ba42fb5b77c8b77fdebdbbb5597dee064
Blur <danielthesmartypants2017@gmail.com>
2024-06-11 00:37:19 -0500

    fix ERRRR

--- src/Lua/init/gametype.lua
+++ src/Lua/init/gametype.lua
@@ -67,9 +67,9 @@ function ZE2:Copy(orig)
     if orig_type == 'table' then
         copy = {}
         for orig_key, orig_value in next, orig, nil do
-            copy[deepcopy(orig_key)] = ZE2:Copy(orig_value)
+            copy[ZE2:Copy(orig_key)] = ZE2:Copy(orig_value)
         end
-        setmetatable(copy, deepcopy(getmetatable(orig)))
+        setmetatable(copy, ZE2:Copy(getmetatable(orig)))
     else -- number, string, boolean, etc
         copy = orig
     end
