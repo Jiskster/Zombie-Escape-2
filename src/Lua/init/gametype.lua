6f4302e6b182d910ede2a4199012b57d47681080
Blur <danielthesmartypants2017@gmail.com>
2024-06-11 00:32:15 -0500

    switch to deep copy

--- src/Lua/init/gametype.lua
+++ src/Lua/init/gametype.lua
@@ -66,9 +66,10 @@ function ZE2:Copy(orig)
     local copy
     if orig_type == 'table' then
         copy = {}
-        for orig_key, orig_value in pairs(orig) do
-            copy[orig_key] = orig_value
+        for orig_key, orig_value in next, orig, nil do
+            copy[deepcopy(orig_key)] = deepcopy(orig_value)
         end
+        setmetatable(copy, deepcopy(getmetatable(orig)))
     else -- number, string, boolean, etc
         copy = orig
     end
