9a03af243511c8cf676d7822a2faeeb79c88e84f
Blur <danielthesmartypants2017@gmail.com>
2024-06-12 21:02:04 -0500

    fix print in z_sendrubies

--- src/Lua/main/capitalism.lua
+++ src/Lua/main/capitalism.lua
@@ -265,7 +265,7 @@ COM_AddCommand("z_sendrubies", function(player, player2, rubies)
 	players[player2]["ze2_info"].rubies = $ + rubies
 	
 	CONS_Printf(player, 
-	"\x82You sent "..player["ze2_info"].rubies.." rubies to "..players[player2].name)
+	"\x82You sent "..rubies.." rubies to "..players[player2].name)
 	CONS_Printf(players[player2], 
 	string.format("\x82%s\x82 sent you %s rubies", player.name, tostring(rubies))
 	)
