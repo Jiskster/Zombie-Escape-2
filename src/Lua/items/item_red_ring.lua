542997cb570f50aa01ab7bd906c197a410d9f07b
Blur <danielthesmartypants2017@gmail.com>
2024-06-14 08:12:37 -0500

    balance red ring

--- src/Lua/items/item_red_ring.lua
+++ src/Lua/items/item_red_ring.lua
@@ -1,15 +1,8 @@
 ZE2:CreateItem("Red Ring",  {
 	object = MT_REDRING,
 	icon = "RINGIND",
-	firerate = 9,
+	firerate = 8,
 	color = SKINCOLOR_RED,
-	knockback = 45*FRACUNIT,
+	knockback = 25*FRACUNIT,
 	damage = 15,
-	onspawn = function(pmo, mo)
-		local mult = (2*FU)
-		
-		mo.momx = FixedMul($,mult)
-		mo.momy = FixedMul($,mult)
-		mo.momz = FixedMul($,mult)
-	end,
 })
