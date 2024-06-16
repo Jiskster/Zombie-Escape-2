bab5861dfb36320d076fb981495cdf752d0870e0
Jisk <37682565+Jiskster@users.noreply.github.com>
2024-06-15 23:50:19 -0500

    use damage fade animation for mirrored players

--- src/Lua/items/item_ws_mirror.lua
+++ src/Lua/items/item_ws_mirror.lua
@@ -17,7 +17,7 @@ local function flashpmo(pmo, source)
 	local thok = P_SpawnMobjFromMobj(source,0,0,0,MT_THOK)
 	thok.color = source.color
 	thok.fuse = 17
-	P_FlashPal(pmo.player, 3, 5*TICRATE)
+	ZE2:SetDamageFadeAnim(pmo.player, 5*TICRATE)
 	P_Thrust(pmo, source.angle, 180*FRACUNIT)
 	S_StartSound(pmo, sfx_bewar2)
 	P_SetScale(thok,thok.scale*3)
