5e9b0f685eb878ef8f6989ec1c1ab3856a2dba5d
Blur <danielthesmartypants2017@gmail.com>
2024-06-22 23:11:16 -0500

    End character selection when no ring

--- src/Lua/main/timers.lua
+++ src/Lua/main/timers.lua
@@ -40,6 +40,17 @@ local countdown_sfx = {
 	[1] = sfx_cone,
 }
 
+local function ForceEndAllCharacterSelection()
+	for player in players.iterate do
+		if player.spectator then continue end
+		
+		if player["ze2_info"].charselect_choosing == true then -- get tf out of character select
+			local selection_name = ZE2.getSkinNames(player, true)[player["ze2_info"].charselect_selection]
+			ZE2.pickcharinselect(player,selection_name) 
+		end
+	end
+end
+
 local function CheckGameForWinRing()
 	local haswinring = false
 	local winring_doomednum = mobjinfo[MT_CRRING].doomednum
@@ -53,6 +64,7 @@ local function CheckGameForWinRing()
 	
 	if not haswinring then
 		print("Mapnum " .. gamemap .. " doesn't have an exit. Forcing zombie win.")
+		ForceEndAllCharacterSelection()
 		ZE2:StartWin(2)
 		return false
 	end
