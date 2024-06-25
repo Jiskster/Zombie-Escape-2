edb11957d1ebdce3ffde8bf7cdc9f74a63015326
Blur <danielthesmartypants2017@gmail.com>
2024-06-25 02:25:56 -0500

    Remove special stage map

--- src/init.lua
+++ src/init.lua
@@ -74,7 +74,6 @@ dofile "hooks/hud.lua"
 dofile "main/name_tags.lua"
 
 dofile "levelscripts/minecraft.lua"
-dofile "levelscripts/specialstage.lua"
 dofile "levelscripts/undertale.lua"
 dofile "levelscripts/caveofthepast.lua"
 
