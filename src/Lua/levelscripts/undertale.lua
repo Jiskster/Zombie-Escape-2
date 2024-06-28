789c06079dd914b1fdef4b633c8982e1d7f61af7
Blur <danielthesmartypants2017@gmail.com>
2024-06-28 02:01:17 -0500

    Fix toriel hp

--- src/Lua/levelscripts/undertale.lua
+++ src/Lua/levelscripts/undertale.lua
@@ -14,7 +14,7 @@ freeslot("MT_TORIEL") -- fuck you SOC
 
 mobjinfo[MT_TORIEL].npc_name = "Toriel"
 mobjinfo[MT_TORIEL].npc_name_color = SKINCOLOR_WHITE
-mobjinfo[MT_TORIEL].npc_spawnhealth = {100,100}
+mobjinfo[MT_TORIEL].npc_spawnhealth = {1000,2500}
 
 local function undertale_floweytalk()
 	chatprint("\x82\<Flowey>\x80 Hey, why are you in a hurry- Wait!")
