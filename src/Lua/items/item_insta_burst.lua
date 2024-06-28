0f1f0bd8c3335a2b66a36a3dd745f62f8d84d121
Blur <danielthesmartypants2017@gmail.com>
2024-06-28 02:05:50 -0500

    increase instaburst delay

--- src/Lua/items/item_insta_burst.lua
+++ src/Lua/items/item_insta_burst.lua
@@ -41,7 +41,7 @@ states[S_INSTABURST6B] = {SPR_NULL, 0, 1, A_CapeChase, 0, 0, S_NULL}
 
 ZE2:CreateItem("Insta Burst", {
 	icon = "ZMISHIND",
-	firerate = 24,
+	firerate = 35,
 	sound = sfx_zish1,
 	damage = 20,
 	color = SKINCOLOR_RED,
