90f547dca3ac5412b1342052c26aba168939127c
Blur <danielthesmartypants2017@gmail.com>
2024-06-28 02:05:19 -0500

    revert instaburst damage to 20

--- src/Lua/items/item_insta_burst.lua
+++ src/Lua/items/item_insta_burst.lua
@@ -43,7 +43,7 @@ ZE2:CreateItem("Insta Burst", {
 	icon = "ZMISHIND",
 	firerate = 24,
 	sound = sfx_zish1,
-	damage = 15,
+	damage = 20,
 	color = SKINCOLOR_RED,
 	ontrigger = function(player)
 		local instaburst = P_SpawnMobjFromMobj(player.mo, 0, 0, 0, MT_INSTABURST)
