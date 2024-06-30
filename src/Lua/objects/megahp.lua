ff1181f005e62e629f01d4c84e8c58a4fae87eff
Blur <danielthesmartypants2017@gmail.com>
2024-06-29 23:07:30 -0500

    MegaHP orb now gives pity shield

--- src/Lua/objects/megahp.lua
+++ src/Lua/objects/megahp.lua
@@ -29,8 +29,8 @@ sfxinfo[sfx_maxhp].caption="Max HP increased"
 ZE2.HitMegaHP = function(special, toucher)
 	if toucher and toucher.valid and toucher.player and toucher.player["ze2_info"].team and toucher.player["ze2_info"].team == 1 then
 		local rubies_given = 45
-		toucher.maxhealth = $ + 60
 		toucher.health = toucher.maxhealth
+		ZE2:GiveShieldToMobj(toucher, 1)
 		ZE2:GivePlayerRubies(toucher.player, rubies_given)
 		CONS_Printf(toucher.player,"\x85+"..rubies_given.." ruby bonus!")
 	else
