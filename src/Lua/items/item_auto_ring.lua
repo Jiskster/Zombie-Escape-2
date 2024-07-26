9a3f98614d17fea9f389320fcc9ca2261f255016
Blur <danielthesmartypants2017@gmail.com>
2024-07-26 02:12:53 -0500

    balance auto ring

--- src/Lua/items/item_auto_ring.lua
+++ src/Lua/items/item_auto_ring.lua
@@ -1,21 +1,21 @@
 local auto_ring = ZE2:CreateItem("Automatic Ring",  {
 	object = MT_THROWNAUTOMATIC,
 	icon = "AUTOIND",
-	firerate = 2,
+	firerate = 3,
 	color = SKINCOLOR_GREEN,
 	autouse = true,
-	damage = 12,
-	knockback = FRACUNIT/3,
+	damage = 20,
+	knockback = FRACUNIT/2,
 	flags2 = MF2_AUTOMATIC,
 	ammo = 100,
 	reload_time = TICRATE*3,
 	price = 50,
 	skin_overwrite = {
 		["knuckles"] = {
-			damage = 30,
+			damage = 40,
 			max_ammo = 150,
 			ammo = 150,
-			firerate = 1,
+			firerate = 2,
 			reload_time = TICRATE*6,
 		}
 	}
