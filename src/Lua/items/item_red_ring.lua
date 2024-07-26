6be454d426cb9e32c1f1afcd8bd9429ff6532f7b
Blur <danielthesmartypants2017@gmail.com>
2024-07-26 14:33:39 -0500

    red ring now has ammo

--- src/Lua/items/item_red_ring.lua
+++ src/Lua/items/item_red_ring.lua
@@ -6,6 +6,9 @@ ZE2:CreateItem("Red Ring",  {
 	knockback = 5*FRACUNIT,
 	damage = 45,
 	velocity_multiplier = 2*FRACUNIT,
+	max_ammo = 50,
+	ammo = 50,
+	reload_time = 1*TICRATE + TICRATE/2,
 	skin_overwrite = {
 		["fang"] = {
 			displayname = "Cork",
