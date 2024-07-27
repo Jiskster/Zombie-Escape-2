bd468a2896c09a151c5795ebb04a0bce640287ac
Jisk <37682565+Jiskster@users.noreply.github.com>
2024-07-27 18:25:36 -0500

    tweak energy drink

--- src/Lua/items/item_energy_drink.lua
+++ src/Lua/items/item_energy_drink.lua
@@ -11,10 +11,10 @@ local energydrink = ZE2:CreateItem("Energy Drink", {
 	color = SKINCOLOR_MASTER,
 	ontrigger = function(player)
 		ZE2:GivePlayerEffect(player, "Energy_Drink", {
-			normalspeed_multiplier = 3*FRACUNIT/2,
-			damage_multiplier = 3*FRACUNIT/2,
-			knockback_multiplier = 3*FRACUNIT,
-		}, 7*TICRATE, true)
+			normalspeed_multiplier = 10*FRACUNIT/7,
+			damage_multiplier = 2*FRACUNIT,
+			knockback_multiplier = 5*FRACUNIT,
+		}, 6*TICRATE, true)
 	end,
 	price = 115,
 })
