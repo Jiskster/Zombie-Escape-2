edf2f5ff90ad7ca6818ce83c858da725bb2037a1
Jisk <37682565+Jiskster@users.noreply.github.com>
2024-07-27 08:36:18 -0500

    remove knockback from enemies

--- src/Lua/enemies/vanilla.lua
+++ src/Lua/enemies/vanilla.lua
@@ -18,8 +18,6 @@ mobjinfo[MT_BLUECRAWLA].npc_name_color = SKINCOLOR_BLUE
 mobjinfo[MT_BLUECRAWLA].rubydrop = {2,4}
 mobjinfo[MT_BLUECRAWLA].painsound = sfx_dmpain
 mobjinfo[MT_BLUECRAWLA].forcedamage = 10
-mobjinfo[MT_BLUECRAWLA].forceknockback = 30*FU
-mobjinfo[MT_BLUECRAWLA].forceverticalknockback = 10*FU
 mobjinfo[MT_BLUECRAWLA].relativeknockback = true
 
 mobjinfo[MT_REDCRAWLA].npc_name = "Red Crawla"
@@ -28,8 +26,6 @@ mobjinfo[MT_REDCRAWLA].npc_name_color = SKINCOLOR_RED
 mobjinfo[MT_REDCRAWLA].rubydrop = {3,5}
 mobjinfo[MT_REDCRAWLA].painsound = sfx_dmpain
 mobjinfo[MT_REDCRAWLA].forcedamage = 20
-mobjinfo[MT_REDCRAWLA].forceknockback = 50*FU -- relativeknockback
-mobjinfo[MT_REDCRAWLA].forceverticalknockback = 10*FU
 mobjinfo[MT_REDCRAWLA].relativeknockback = true
 
 mobjinfo[MT_GOLDCRAWLA] = {
@@ -67,18 +63,12 @@ mobjinfo[MT_GOLDBUZZ].npc_spawnhealth = {3,8}
 mobjinfo[MT_GOLDBUZZ].npc_name_color = SKINCOLOR_GOLD
 mobjinfo[MT_GOLDBUZZ].rubydrop = {1,3}
 mobjinfo[MT_GOLDBUZZ].painsound = sfx_dmpain
-mobjinfo[MT_GOLDBUZZ].forcedamage = 4
-mobjinfo[MT_GOLDBUZZ].forceknockback = 10*FU
-mobjinfo[MT_GOLDBUZZ].relativeknockback = true
 
 mobjinfo[MT_REDBUZZ].npc_name = "Red Buzz"
 mobjinfo[MT_REDBUZZ].npc_spawnhealth = {10,17}
 mobjinfo[MT_REDBUZZ].npc_name_color = SKINCOLOR_RED
 mobjinfo[MT_REDBUZZ].rubydrop = {2,4}
 mobjinfo[MT_REDBUZZ].painsound = sfx_dmpain
-mobjinfo[MT_REDBUZZ].forcedamage = 6
-mobjinfo[MT_REDBUZZ].forceknockback = 15*FU
-mobjinfo[MT_REDBUZZ].relativeknockback = true
 
 mobjinfo[MT_PENGUINATOR].npc_name = "Penguinator"
 mobjinfo[MT_PENGUINATOR].npc_spawnhealth = {10,20}
@@ -86,28 +76,18 @@ mobjinfo[MT_PENGUINATOR].npc_name_color = SKINCOLOR_ICY
 mobjinfo[MT_PENGUINATOR].rubydrop = {5,8}
 mobjinfo[MT_PENGUINATOR].painsound = sfx_dmpain
 mobjinfo[MT_PENGUINATOR].forcedamage = 5
-mobjinfo[MT_PENGUINATOR].forceknockback = 30*FU -- relativeknockback
-mobjinfo[MT_PENGUINATOR].forceverticalknockback = 20*FU
-mobjinfo[MT_PENGUINATOR].relativeknockback = true
 
 mobjinfo[MT_GOOMBA].npc_name = "Goomba"
 mobjinfo[MT_GOOMBA].npc_spawnhealth = {10,15}
 mobjinfo[MT_GOOMBA].npc_name_color = SKINCOLOR_ORANGE
 mobjinfo[MT_GOOMBA].rubydrop = {2,2}
 mobjinfo[MT_GOOMBA].forcedamage = 10
-mobjinfo[MT_GOOMBA].forceknockback = 20*FU -- relativeknockback
-mobjinfo[MT_GOOMBA].forceverticalknockback = 6*FU
-mobjinfo[MT_GOOMBA].relativeknockback = true
 
 mobjinfo[MT_BLUEGOOMBA].npc_name = "Blue Goomba"
 mobjinfo[MT_BLUEGOOMBA].npc_spawnhealth = {10,15}
 mobjinfo[MT_BLUEGOOMBA].npc_name_color = SKINCOLOR_BLUE
 mobjinfo[MT_BLUEGOOMBA].rubydrop = {2,2}
 mobjinfo[MT_BLUEGOOMBA].forcedamage = 10
-mobjinfo[MT_BLUEGOOMBA].forceknockback = 20*FU -- relativeknockback
-mobjinfo[MT_BLUEGOOMBA].forceverticalknockback = 6*FU
-mobjinfo[MT_BLUEGOOMBA].relativeknockback = true
-
 /*
 local function GoldCrawlaRNG(mobj)
 	if gametype ~= GT_ZE2 then return end
