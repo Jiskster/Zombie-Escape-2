6e0fab5e14786160c768c6b7e5f77bc7b74bc199
Blur <danielthesmartypants2017@gmail.com>
2024-07-31 21:53:02 -0500

    Decrease ground acceleration

--- src/Lua/main/characterconfigs.lua
+++ src/Lua/main/characterconfigs.lua
@@ -132,7 +132,7 @@ ZE2.SetCCtoplayer = function(player)
 
 		if P_IsObjectOnGround(pmo) or
 		(not P_IsObjectOnGround(pmo) and cmd.forwardmove < 0 and P_GetPlayerControlDirection(player) == 2) then
-			player.thrustfactor = 10
+			player.thrustfactor = 8
 		else
 			player.thrustfactor = 4
 		end
@@ -240,7 +240,7 @@ ZE2.SetZCtoplayer = function(player)
 			
 			if P_IsObjectOnGround(pmo) or
 			(not P_IsObjectOnGround(pmo) and cmd.forwardmove < 0 and P_GetPlayerControlDirection(player) == 2) then
-				player.thrustfactor = 10
+				player.thrustfactor = 8
 			else
 				player.thrustfactor = 4
 			end
