eef528e2ae30251efb3750f6c07888ad8973025a
Blur <danielthesmartypants2017@gmail.com>
2024-07-31 21:57:23 -0500

    Balance Red Ring

--- src/Lua/items/item_red_ring.lua
+++ src/Lua/items/item_red_ring.lua
@@ -1,9 +1,9 @@
 ZE2:CreateItem("Red Ring",  {
 	object = MT_REDRING,
 	icon = "RINGIND",
-	firerate = 5,
+	firerate = 6,
 	color = SKINCOLOR_RED,
-	knockback = 15*FRACUNIT,
+	knockback = 7*FRACUNIT,
 	damage = 45,
 	velocity_multiplier = 2*FRACUNIT,
 	max_ammo = 50,
