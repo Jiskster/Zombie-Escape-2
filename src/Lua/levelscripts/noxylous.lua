f34b81fb7cb7d8d61a58d1e575fe5dc33e122ce8
Blur <danielthesmartypants2017@gmail.com>
2024-11-18 22:34:53 -0600

    Update NOXY map.

--- src/Lua/levelscripts/noxylous.lua
+++ src/Lua/levelscripts/noxylous.lua
@@ -14,6 +14,14 @@ local NOXY_Timer2 = ZE2:AddTimer("Defend Final Barrier",{
 	extrainfo = {color = SKINCOLOR_PURPLE},
 })
 
+local NOXY_Timer3 = ZE2:AddTimer("Leaving Platform",{
+	time = 20*TICRATE,
+	on_end = function(timernum,timername)
+		P_LinedefExecute(23)
+	end,
+	extrainfo = {color = SKINCOLOR_RED},
+})
+
 addHook("LinedefExecute", function()
 	NOXY_Timer1.active = true
 end, "NOXYTIMER1")
@@ -21,3 +29,7 @@ end, "NOXYTIMER1")
 addHook("LinedefExecute", function()
 	NOXY_Timer2.active = true
 end, "NOXYTIMER2")
+
+addHook("LinedefExecute", function()
+	NOXY_Timer3.active = true
+end, "NOXYTIMER3")
