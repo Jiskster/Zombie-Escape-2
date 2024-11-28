846392a807418d226a042b684216f6fd17581d8e
Blur <danielthesmartypants2017@gmail.com>
2024-11-27 19:17:05 -0600

    increase cash cap

--- src/Lua/main/baseplayer.lua
+++ src/Lua/main/baseplayer.lua
@@ -2,7 +2,7 @@ freeslot("sfx_zjump")
 sfxinfo[sfx_zjump].caption = "Jump"
 mobjinfo[MT_LHRT].forceknockback = 20*FRACUNIT
 ZE2.JumpSprintFatigue = 21*FRACUNIT
-ZE2.DefaultRubyCap = 10000;
+ZE2.DefaultRubyCap = 25000;
 ZE2.RubyStart = 500 -- the amount of rubies you start when you join a server
 
 ZE2.Effect_ThinkerFuncs = {
