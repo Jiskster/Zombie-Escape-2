81f9e626c1baca6a3e394c122c17fe67b2be0a17
Blur <danielthesmartypants2017@gmail.com>
2025-02-03 22:53:30 -0600

    Remove leftover comments

--- src/Lua/hud/setup.lua
+++ src/Lua/hud/setup.lua
@@ -26,32 +26,6 @@ local function SetupHud(filename)
 	)
 end
 
---gametype ~= GT_ZE2
-
-/*
-customhud.SetupItem("ze2_pregame", ze2_modname, ZE2.pregamehud, "game", 0)
-customhud.SetupItem("ze2_characterselect", ze2_modname, ZE2.characterselecthud, "game", 0)
-
-customhud.SetupItem("ze2_mapinfo", ze2_modname, ZE2.mapinfohud, "game", 0)
-
-customhud.SetupItem("ze2_nametags", ze2_modname, ZE2.nametagshud, "game", 0)
-
-customhud.SetupItem("ze2_damageindicator", ze2_modname, ZE2.damageindicatorhud, "game", 4)
-
-customhud.SetupItem("ze2_damagefade", ze2_modname, ZE2.damagefadehud, "game", 0)
-
-customhud.SetupItem("ze2_info", ze2_modname, ZE2.infohud, "game", 0)
-
-customhud.SetupItem("ze2_toggle", ze2_modname, ZE2.togglehud, "game", 0)
-customhud.SetupItem("ze2_intermissionhud", ze2_modname, ZE2.intermissionhud, "game", 0)
-customhud.SetupItem("ze2_shop", ze2_modname, ZE2.shophud, "game", 0)
-customhud.SetupItem("ze2_zombieshop", ze2_modname, ZE2.zombie_shophud, "game", 0)
-customhud.SetupItem("ze2_inventory", ze2_modname, ZE2.inventoryhud, "game", 0)
-customhud.SetupItem("ze2_scores", ze2_modname, ZE2.tabscores, "scores", 0)
-
-customhud.SetupItem("ze2_debug", ze2_modname, ZE2.debughud, "game", 1)
-*/
-
 SetupHud "HudToggle"
 
 SetupHud "PreGame"
