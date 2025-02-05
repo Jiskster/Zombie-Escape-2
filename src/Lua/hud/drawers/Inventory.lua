609d0c479515b188f1b5c423d4d281aea9d5c483
Blur <danielthesmartypants2017@gmail.com>
2025-02-05 01:03:20 -0600

    Inventory Hud Rewrite (WIP)

--- src/Lua/hud/drawers/Inventory.lua
+++ src/Lua/hud/drawers/Inventory.lua
@@ -1,3 +1,19 @@
+local invpos_y = 185*FU
+local empty_patch_name = "BLANKIND"
+local slot_gap = 20*FU
+
+local function PositionSlot(index, slot, slot_count, slot_gap)
+	-- Center Slot Position
+	slot.x = $ - (slot.patch.width*FU)/2
+	slot.y = $ - (slot.patch.height*FU)/2
+	
+	-- Add from the right
+	slot.x = $ + (slot_gap) * (index-1)
+	--slot.x = $ + slot_gap * (index-1)
+	-- Center whole inventory
+	slot.x = $ - (slot_gap/2) * (slot_count-1)
+end
+
 return "Inventory", function(v, player)
 	if player["ze2_info"].pregamemenu_active then return end
 	if ZE2.game_ended then return end
@@ -9,5 +25,69 @@ return "Inventory", function(v, player)
 	
 	if player and not player.mo then return end
 	
-	v.drawString(160, 170, "WIP INVENTORY", V_SNAPTOBOTTOM, "center")
+	local slot_count = ZE2:FetchInventoryLimit(player)
+	
+	local selected_slot = {
+		x = (BASEVIDWIDTH*FU)/2, -- In middle of screen
+		y = invpos_y,
+		scale = FRACUNIT,
+		patch = v.cachePatch("CURWEAP"),
+		flags = V_SNAPTOBOTTOM,
+		positioned_selection = false, -- Has the selected slot been found to render?
+	}
+	
+	for i=1, slot_count do
+		local selection = player["ze2_info"].inventory_selection
+		
+		local slot = {
+			x = (BASEVIDWIDTH*FU)/2, -- In middle of screen
+			y = invpos_y,
+			scale = FRACUNIT,
+			patch = v.cachePatch(empty_patch_name),
+			flags = V_SNAPTOBOTTOM,
+		}
+		
+		local iteminfo = ZE2:FetchInventorySlot(player, i)
+		local item_icon = ZE2:GetItemInfoIndex(iteminfo, "icon", player.mo.skin)
+		
+		if item_icon then
+			slot.patch = v.cachePatch(item_icon)
+		end
+		
+		if selection == i then
+			PositionSlot(i,
+				selected_slot, 
+				slot_count, 
+				slot_gap
+			)
+			
+			selected_slot.positioned_selection = true
+		end
+		
+		PositionSlot(i, 
+			slot, 
+			slot_count, 
+			slot_gap
+		)
+		
+		v.drawScaled(
+			slot.x,
+			slot.y,
+			slot.scale,
+			slot.patch,
+			slot.flags
+		)
+	end
+	
+	if selected_slot.positioned_selection then
+		local selection = player["ze2_info"].inventory_selection
+		
+		v.drawScaled(
+			selected_slot.x,
+			selected_slot.y,
+			selected_slot.scale,
+			selected_slot.patch,
+			selected_slot.flags
+		)
+	end
 end
