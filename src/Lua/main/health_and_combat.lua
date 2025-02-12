91d31b07d7595a6778e158e5b54a2c711b6ec594
Blur <danielthesmartypants2017@gmail.com>
2025-02-11 21:17:34 -0600

    Change up zombie knockback physics.
    Use relative thrusting in air

--- src/Lua/main/health_and_combat.lua
+++ src/Lua/main/health_and_combat.lua
@@ -435,10 +435,13 @@ addHook("ShouldDamage", function(mo, inf, src, dmg, damagetype)
 			
 			if not relativeknockback then
 				local r_momxy = FixedHypot(mo.momx, mo.momy)
+
+				if P_IsObjectOnGround(mo) then
+					P_InstaThrust(mo, inf.angle, knockback + r_momxy)
+				else
+					P_Thrust(mo, inf.angle, knockback)
+				end
 				
-				--P_Thrust(mo, inf.angle, knockback)
-				
-				P_InstaThrust(mo, inf.angle, knockback + r_momxy)
 				mo.player["ze2_info"].nofrictiontics = min($ + 3, 5)
 			else
 				local r_angle = R_PointToAngle2(mo.x, mo.y, inf.x, inf.y)
