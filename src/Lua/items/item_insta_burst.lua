2756833271b2ced89d42e0659f86e098a9fb602a
Jisk <danielthesmartypants2017@gmail.com>
2025-05-22 21:53:45 -0500

    Nerf instaburst time

--- src/Lua/items/item_insta_burst.lua
+++ src/Lua/items/item_insta_burst.lua
@@ -41,7 +41,7 @@ states[S_INSTABURST6B] = {SPR_NULL, 0, 1, A_CapeChase, 0, 0, S_NULL}
 
 ZE2:CreateItem("Insta Burst", {
 	icon = "ZMISHIND",
-	firerate = 14,
+	firerate = 28,
 	sound = {sfx_zish1, sfx_zish2, sfx_zish3},
 	damage = 50,
 	color = SKINCOLOR_RED,
