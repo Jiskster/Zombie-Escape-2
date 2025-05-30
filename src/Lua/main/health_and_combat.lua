7212806c6c6018a4dc534b34e1e8c20e61e65e52
luigi-budd <90123055+luigi-budd@users.noreply.github.com>
2025-05-30 17:06:04 -0500

    Minor tweaks

--- src/Lua/main/health_and_combat.lua
+++ src/Lua/main/health_and_combat.lua
@@ -206,7 +206,7 @@ function ZE2:AddDamageIndicator(player, victim_mobj, damage)
 		player.ze2.damage_indicator_table[victim_mobj] = {
 			tics_left = TICRATE*2,
 			animation = 1,
-			number = damage,
+			number = max(damage, victum_mobj.health),
 			draw_x = victim_mobj.x,
 			draw_y = victim_mobj.y,
 			draw_z = victim_mobj.z + (victim_mobj.height*2),
