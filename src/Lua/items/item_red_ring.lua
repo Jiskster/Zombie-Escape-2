70429d1f9ffcb8a5cf78a6e34cabf188ca2a974f
Jisk <danielthesmartypants2017@gmail.com>
2025-06-02 06:10:39 -0500

    nerf red ring knockback

--- src/Lua/items/item_red_ring.lua
+++ src/Lua/items/item_red_ring.lua
@@ -26,7 +26,7 @@ ZE2:CreateItem("red_ring",  {
 	sound = sfx_wpfire,
 	firerate = 4,
 	color = SKINCOLOR_RED,
-	knockback = 7*FRACUNIT,
+	knockback = 5*FRACUNIT,
 	damage = 45,
 	velocity_precision = 8,
 	velocity_multiplier = 8*FRACUNIT,
