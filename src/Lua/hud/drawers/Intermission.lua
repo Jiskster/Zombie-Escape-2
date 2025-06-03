32b6a6a4353fdefa2b7f684838b469f5c0790c78
Jisk <danielthesmartypants2017@gmail.com>
2025-06-03 18:06:47 -0500

    WIP new win screen and music

--- src/Lua/hud/drawers/Intermission.lua
+++ src/Lua/hud/drawers/Intermission.lua
@@ -11,6 +11,29 @@ local ty = CV_RegisterVar({
 
 return "Intermission", function(v, player)
 	if not ZE2.game_ended then return end
-	
+	local wtics = ZE2.win_tics
+	local team_won = ZE2.team_won
 
+	local team_patch = v.cachePatch("Z_SURVIVORS")
+	
+	if team_won == 2 then
+		team_patch = v.cachePatch("Z_ZOMBIES")
+	end
+	
+	local win_patch = v.cachePatch("Z_WIN")
+	
+	local end_goals = { -- fixed x values
+		team = 20*FU; -- come from the left
+		win = 220*FU; -- come from the right
+	}
+	
+	local anim_time = TICRATE + TICRATE/2
+	local diff = anim_time - max(0, anim_time - wtics)
+	local div = FixedDiv(diff*FU, anim_time*FU)
+	
+	local team_x = ease.outquint(div, end_goals.team - 200*FU, end_goals.team)
+	local win_x = ease.outquint(div, end_goals.win + 200*FU, end_goals.win)
+	
+	v.drawScaled(team_x, 100*FU, FU, team_patch)
+	v.drawScaled(win_x, 100*FU, FU, win_patch)
 end
