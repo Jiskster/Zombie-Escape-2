52771d5cff4bbba9e5cbccbaba54515645639cf3
Jisk <danielthesmartypants2017@gmail.com>
2025-06-04 16:46:26 -0500

    Tweak red ring

--- src/Lua/items/item_red_ring.lua
+++ src/Lua/items/item_red_ring.lua
@@ -27,13 +27,13 @@ ZE2:CreateItem("red_ring",  {
 	firerate = 4,
 	color = SKINCOLOR_RED,
 	knockback = 5*FRACUNIT,
-	damage = 45,
+	damage = 25,
 	velocity_precision = 8,
 	velocity_multiplier = 8*FRACUNIT,
 	autouse = false,
-	max_ammo = 25,
-	ammo = 25,
-	reload_time = 3*TICRATE/2,
+	max_ammo = 30,
+	ammo = 30,
+	reload_time = 7*TICRATE/4,
 	skin_overwrite = {
 		["tails"] = {
 			autouse = true
