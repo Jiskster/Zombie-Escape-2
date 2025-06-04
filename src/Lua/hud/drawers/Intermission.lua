2e7505a0e3b68a085e995037c7069e8164ed6a47
Jisk <danielthesmartypants2017@gmail.com>
2025-06-03 21:10:53 -0500

    Add animation and triangles to the win screens

--- src/Lua/hud/drawers/Intermission.lua
+++ src/Lua/hud/drawers/Intermission.lua
@@ -9,18 +9,26 @@ local ty = CV_RegisterVar({
 	PossibleValue = CV_Unsigned,
 })
 
+local wtics2 = 0 -- for triangle animation
+
 return "Intermission", function(v, player)
-	if not ZE2.game_ended then return end
+	if not ZE2.game_ended then
+		wtics2 = 0
+		return
+	end
 	local wtics = ZE2.win_tics
 	local team_won = ZE2.team_won
 
 	local team_patch = v.cachePatch("Z_SURVIVORS")
+	local win_patch = v.cachePatch("Z_WIN")
+	local lower_patch = v.cachePatch("Z_BANG_LOWER_BLUE")
+	local upper_patch = v.cachePatch("Z_BANG_UPPER_BLUE")
 	
 	if team_won == 2 then
 		team_patch = v.cachePatch("Z_ZOMBIES")
+		lower_patch = v.cachePatch("Z_BANG_LOWER_RED")
+		upper_patch = v.cachePatch("Z_BANG_UPPER_RED")
 	end
-	
-	local win_patch = v.cachePatch("Z_WIN")
 	
 	local end_goals = { -- fixed x values
 		team = 20*FU; -- come from the left
@@ -34,6 +42,25 @@ return "Intermission", function(v, player)
 	local team_x = ease.outquint(div, end_goals.team - 200*FU, end_goals.team)
 	local win_x = ease.outquint(div, end_goals.win + 200*FU, end_goals.win)
 	
+	-- animations for making the triangle appear
+	local anim_time2 = 2*TICRATE
+	local div2 = FixedDiv(min(wtics2*FU,anim_time2*FU), anim_time2*FU)
+	local triangle_y = ease.linear(div2, upper_patch.height*FU, 0)
+	
+	if wtics >= 100 then
+		wtics2 = $ + 1
+	end
+	
 	v.drawScaled(team_x, 100*FU, FU, team_patch)
 	v.drawScaled(win_x, 100*FU, FU, win_patch)
+	
+	for i=-3,3 do
+		local x_movement = ((leveltime*FU)%(lower_patch.width*FU))
+		local bottom_x = i*(lower_patch.width*FU) + x_movement
+		local bottom_y = ((200*FU)-lower_patch.height*FU) + triangle_y
+		local top_x = i*(upper_patch.width*FU) - x_movement
+		local top_y = 0 - triangle_y -- could do -triangle_y but this is visually better
+		v.drawScaled(top_x, top_y, FU, upper_patch, V_SNAPTOTOP)
+		v.drawScaled(bottom_x, bottom_y, FU, lower_patch, V_SNAPTOBOTTOM)
+	end
 end
