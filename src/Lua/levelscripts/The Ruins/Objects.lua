24579b2eb504c09c8f2c1add39adeb61b4d47e5a
Jisk <danielthesmartypants2017@gmail.com>
2025-06-10 06:01:54 -0500

    "The ruins" map fixes.
    When you reach a pushable puzzle area, it automatically activates a platform after 30 seconds.
    
    Fixed flowey spamming chat.
    
    Buffed toriels' hp heavily

--- src/Lua/levelscripts/The Ruins/Objects.lua
+++ src/Lua/levelscripts/The Ruins/Objects.lua
@@ -108,7 +108,7 @@ mobjinfo[MT_TORIEL] = {
 
 mobjinfo[MT_TORIEL].npc_name = "Toriel"
 mobjinfo[MT_TORIEL].npc_name_color = SKINCOLOR_WHITE
-mobjinfo[MT_TORIEL].npc_spawnhealth = {6500,9500}
+mobjinfo[MT_TORIEL].npc_spawnhealth = {10000,20000}
 --mobjinfo[MT_TORIEL].rubydrop = {30,60}
 mobjinfo[MT_TORIEL].forcedamage = 1000
 mobjinfo[MT_TORIEL].antiknockback = true
