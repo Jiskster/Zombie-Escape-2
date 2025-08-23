89fe644402a6a19e60dc175f11539b9e58984b25
luigi-budd <90123055+luigi-budd@users.noreply.github.com>
2025-08-22 23:14:25 -0500

    No quick stamina charge when midair

--- src/Lua/hooks/PreThinkFrame/Sprint.lua
+++ src/Lua/hooks/PreThinkFrame/Sprint.lua
@@ -6,35 +6,34 @@ return function()
 
 		local pmo = player.mo
 		local cc = ZE2.CharacterConfig
+		local grounded = P_IsObjectOnGround(pmo)
 
 		local increment = FRACUNIT/2
-		local decrement = fixedfromstring("0.3")
+		local decrement = fixedfromstring("0.3") --tofixed?
 
-		if player.ze2.team == 1 then
-			if not player.climbing then
-				if (player.speed/FU) > 12 then -- running
-					local run = false
-					if P_IsObjectOnGround(pmo) and not player.ze2.crouching and player.ze2.isRunning then
-						P_SpawnSkidDust(player, 20*FRACUNIT)
-						player.ze2:ChangeStamina(-decrement)
-						player.runspeed = 1
-						run = true
-					end
-					
-					if not run then
-						player.ze2:ChangeStamina(increment/2)
-						player.runspeed = 32000*FRACUNIT
-					end
-				else
-					if not (player.speed/FU) then -- not moving
-						player.ze2:ChangeStamina(increment*3)
-					else -- moving but slower than running speed
-						player.ze2:ChangeStamina(increment)
-					end
-					
-					player.runspeed = 32000*FRACUNIT
-				end
+		if player.ze2.team ~= 1 then continue end
+		if player.climbing then continue end
+
+		if (player.speed/FU) > 12 then -- running
+			local run = false
+			if grounded and not player.ze2.crouching and player.ze2.isRunning then
+				P_SpawnSkidDust(player, 20*FRACUNIT)
+				player.ze2:ChangeStamina(-decrement)
+				player.runspeed = 1
+				run = true
 			end
+			
+			if not run then
+				player.ze2:ChangeStamina(increment/2)
+				player.runspeed = 32000*FRACUNIT
+			end
+		else
+			if (player.speed/FU == 0) and grounded then -- not moving
+				player.ze2:ChangeStamina(increment*3)
+			else -- moving but slower than running speed
+				player.ze2:ChangeStamina(increment)
+			end
+			player.runspeed = 32000*FRACUNIT
 		end
 	end
 end
