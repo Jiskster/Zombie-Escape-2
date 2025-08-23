68faa9ab7c9812e9a4680064402bd154379b3017
luigi-budd <90123055+luigi-budd@users.noreply.github.com>
2025-08-22 20:22:56 -0500

    Add `skin` field to zombie configs

--- src/Lua/hooks/PlayerThink/Skin&ColorLock.lua
+++ src/Lua/hooks/PlayerThink/Skin&ColorLock.lua
@@ -4,11 +4,27 @@ return function(player)
 	local ztype = player.ze2.zombie_type
 	local zc = ZE2.ZombieConfig
 
-	if player.ze2.team == 2 and player.mo.skin ~= "zzombie" then
-		R_SetPlayerSkin(player, "zzombie")
-	elseif player.ze2.team == 1 and player.mo.skin == "zzombie" then
-		R_SetPlayerSkin(player, "sonic")
-		player.mo.color = player.skincolor
+	if player.ze2.team == 2 then
+		local zskin = "zzombie"
+		if (ztype and zc[ztype]) then
+			zskin = zc[ztype].skin
+		end
+		local notzombie = skins[player.skin].name ~= zskin
+		if notzombie then
+			R_SetPlayerSkin(player, zskin)
+		end
+	elseif player.ze2.team == 1 then
+		local badskin = skins[player.skin].name == "zzombie"
+		for name,_ in pairs(zc) do
+			if skins[player.skin].name == name then
+				badskin = true
+				break
+			end
+		end
+		if badskin then
+			R_SetPlayerSkin(player, "sonic")
+			player.mo.color = player.skincolor
+		end
 	end
 		
 	if (player.ze2.team == 2 and ztype and zc[ztype]) then 
--- src/Lua/main/characterconfigs.lua
+++ src/Lua/main/characterconfigs.lua
@@ -4,6 +4,7 @@ ZE2.StandardJumpFactor = FixedDiv(90*FU, 100*FU)
 --			so we can be sure there are no "holes" in them? Maybe `newentry.__index = ZE2.CharacterConfig["default"]`
 ZE2.ZombieConfig = {
 	["normal"] = {
+		skin = "zzombie",
 		skincolor = SKINCOLOR_MOSS,
 		normalspeed = 18 * FRACUNIT,
 		health = 10000,
@@ -30,6 +31,7 @@ ZE2.ZombieConfig = {
 		}
 	},
 	["alpha"] = {
+		skin = "zzombie",
 		skincolor = SKINCOLOR_ALPHAZOMBIE,
 		normalspeed = 17 * FRACUNIT,
 		health = 15000,
