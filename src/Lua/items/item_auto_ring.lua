607e8e055cf7b030ff01629f359b614064c9c9c9
Jisk <danielthesmartypants2017@gmail.com>
2025-08-23 23:04:12 -0500

    Heavily tweak auto ring

--- src/Lua/items/item_auto_ring.lua
+++ src/Lua/items/item_auto_ring.lua
@@ -3,26 +3,25 @@ local auto_ring = ZE2:CreateItem("auto_ring",  {
 	object = MT_THROWNAUTOMATIC,
 	icon = "AUTOIND",
 	sound = sfx_wpfir2,
-	firerate = 2,
+	firerate = 3,
 	color = SKINCOLOR_GREEN,
 	autouse = true,
 	damage = 7,
-	velocity_multiplier = 3*FRACUNIT + 4*FRACUNIT/3,
-	velocity_precision = 4,
+	velocity_multiplier = 2*FRACUNIT,
+	velocity_precision = 2,
 	knockback = 3*FRACUNIT,
 	flags2 = MF2_AUTOMATIC,
-	ammo = 30,
+	ammo = 50,
 	reload_time = TICRATE*2,
 	price = 250,
 	skin_overwrite = {
 		["knuckles"] = {
 			damage = 20,
-			max_ammo = 50,
-			ammo = 50,
+			max_ammo = 100,
+			ammo = 100,
 			firerate = 2,
 			knockback = 5*FRACUNIT,
-			velocity_multiplier = 2*FRACUNIT,
-			reload_time = TICRATE*3,
+			reload_time = TICRATE*4,
 		}
 	}
 })
