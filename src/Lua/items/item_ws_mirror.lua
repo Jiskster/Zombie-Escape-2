80e4cb9d1104d18c5f8ec6e367d10a4feffc2933
luigi-budd <90123055+luigi-budd@users.noreply.github.com>
2025-08-23 23:27:49 -0500

    "Chud Chud Chud" - darnell fnf

--- src/Lua/items/item_ws_mirror.lua
+++ src/Lua/items/item_ws_mirror.lua
@@ -21,7 +21,7 @@ local function flashpmo(pmo, source)
 	thok.fuse = 17
 	pmo.player.ze2:DamageFade(5*TICRATE)
 	local ang = R_PointToDist2(pmo.x, pmo.y, source.x, source.y)
-	KB.addKnockback(mo, 12, ang - ANGLE_180, 180*FRACUNIT)
+	KB.addKnockback(pmo, 12, ang - ANGLE_180, 180*FRACUNIT)
 	pmo.health = 1
 	S_StartSound(pmo, sfx_bewar2)
 	P_SetScale(thok,thok.scale*3)
