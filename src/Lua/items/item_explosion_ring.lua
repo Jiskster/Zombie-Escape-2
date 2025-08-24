609a9b61745f12d251408a8ccec350edf9aef130
Jisk <danielthesmartypants2017@gmail.com>
2025-08-23 20:50:13 -0500

    Nerf explosion ring kb + kb tics

--- src/Lua/items/item_explosion_ring.lua
+++ src/Lua/items/item_explosion_ring.lua
@@ -5,8 +5,8 @@ local explosion_ring = ZE2:CreateItem("explosion_ring", {
 	firerate = TICRATE*3,
 	color = SKINCOLOR_BLACK,
 	damage = 120,
-	knockback = 160*FRACUNIT,
-	knockback_tics = TICRATE*2,
+	knockback = 100*FRACUNIT,
+	knockback_tics = TICRATE,
 	price = 700,
 })
 
