e46e9bc8465ddeba153fdd54165539c1298ece3f
GLide KS <84994993+GLideKS@users.noreply.github.com>
2025-11-24 01:40:21 -0400

    Move MC object related code from minecraft timers
    also mcmapnum is unused

--- src/Lua/ZE2/levelscripts/Minecraft/Sounds_and_Objects.lua
+++ src/Lua/ZE2/levelscripts/Minecraft/Sounds_and_Objects.lua
@@ -1,6 +1,7 @@
 freeslot(
-"sfx_dropen", "sfx_prtal", "sfx_ston", "sfx_trvel", "sfx_mclava", "sfx_mcwatr", "SPR_MCSN", "S_MINECRAFTSUN",
-"MT_MINECRAFTSUN"
+	"sfx_dropen", "sfx_prtal", "sfx_ston", "sfx_trvel", "sfx_mclava", 
+	"sfx_mcwatr", "SPR_MCSN", "S_MINECRAFTSUN",	"MT_MINECRAFTSUN",
+	"MT_MCTORCH","S_MCTORCH","SPR_MCTR", "MT_CREEPER","S_CREEPER","SPR_CRPR"
 )
 
 states[S_MINECRAFTSUN] = {SPR_MCSN, A, -1, nil, 0, 0, S_MINECRAFTSUN}
@@ -16,4 +17,47 @@ mobjinfo[MT_MINECRAFTSUN] = {
         height = 5*FRACUNIT,
         mass = 1,
 		flags = MF_NOTHINK|MF_NOGRAVITY|MF_NOCLIP
+}
+
+mobjinfo[MT_MCTORCH] = {
+	--$Title Minecraft Torch
+	--$Sprite MCTRA0
+	--$Category Minecraft
+	doomednum = 2308,
+	spawnstate = S_MCTORCH,
+	spawnhealth = 100,
+	reactiontime = 8,
+	radius = 8*FRACUNIT,
+	height = 16*FRACUNIT,
+	flags =	MF_NOBLOCKMAP|MF_NOGRAVITY|MF_SCENERY,
+	seestate = S_MCTORCH,
+}
+
+states[S_MCTORCH] = {
+	sprite = SPR_MCTR,
+	frame = A,
+	tics = 0,
+	nextstate = S_MCTORCH,
+}
+
+mobjinfo[MT_CREEPER] = {
+	--$Title Creeper
+	--$Category Minecraft
+	doomednum = 2309,
+	spawnstate = S_CREEPER,
+	spawnhealth = 100,
+	reactiontime = 8,
+	radius = 20*FRACUNIT,
+	height = 85*FRACUNIT,
+	flags =	MF_ENEMY|MF_SPECIAL|MF_SHOOTABLE,
+	seestate = S_CREEPER,
+}
+mobjinfo[MT_CREEPER].npc_name = "Creeper"
+mobjinfo[MT_CREEPER].npc_spawnhealth = {80,80}
+
+states[S_CREEPER] = {
+	sprite = SPR_CRPR,
+	frame = A,
+	tics = 0,
+	nextstate = S_CREEPER,
 }
