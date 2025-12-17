7b21382011b8fb2caf3273f46b283136ede4c5eb
Jisk <danielthesmartypants2017@gmail.com>
2025-12-16 22:46:37 -0600

    nerf decrement again

--- src/Lua/ZE2/hooks/PreThinkFrame/Sprint.lua
+++ src/Lua/ZE2/hooks/PreThinkFrame/Sprint.lua
@@ -9,7 +9,7 @@ return function()
 		local grounded = P_IsObjectOnGround(pmo)
 
 		local increment = FRACUNIT/2
-		local decrement = fixedfromstring("0.6") --tofixed?
+		local decrement = fixedfromstring("0.5") --tofixed?
 
 		if player.ze2.team ~= 1 then continue end
 		if player.climbing then continue end
