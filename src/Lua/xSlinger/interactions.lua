bc8caca0b70c9957a4baaf00af20e829a395d6e0
Jisk <37682565+Jiskster@users.noreply.github.com>
2026-01-26 13:45:54 -0600

    Properly ignore non string item ids on pickup

--- src/Lua/xSlinger/interactions.lua
+++ src/Lua/xSlinger/interactions.lua
@@ -117,9 +117,10 @@ addHook("PlayerThink", function(player)
 					P_RemoveMobj(i_obj.target)
 					P_RemoveMobj(i_obj)
 					
-					if type("itemid") == "string" then
+					if type(itemid) == "string" then
 						xS:give_item(itemid, nil, nil, iteminfo)
 					end
+					
 					xS.interaction_delay = 12	
 				else -- default
 					xS.interaction_delay = 4 -- keep this low
