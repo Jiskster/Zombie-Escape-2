524b0d5686b8559f0c6a753e1055c852ed4ade8c
GLide KS <84994993+GLideKS@users.noreply.github.com>
2026-02-03 20:18:31 -0400

    Try optimize Character Select HUD
    I get +20 fps with this. but the for loop is what are killing the fps a lot.

--- src/Lua/ZE2/hud/drawers/CharacterSelect.lua
+++ src/Lua/ZE2/hud/drawers/CharacterSelect.lua
@@ -1,13 +1,18 @@
 local min = min
 local max = max
-local FRACUNIT = FRACUNIT
 local FU = FU
 local V_ALPHASHIFT = V_ALPHASHIFT
 local V_SNAPTOLEFT = V_SNAPTOLEFT
 local V_SNAPTOTOP = V_SNAPTOTOP
 local FixedDiv = FixedDiv
 local FixedMul = FixedMul
-
+local drawScaled
+local drawString
+local drawFill
+local drawLevelTitle
+local getSprite2Patch
+local getColormap
+local cachePatch
 local select_ease = ease.outquint
 
 
@@ -27,8 +32,8 @@ end
 
 -- tatsuru
 local function FixedPow(a, b)
-    local res = FRACUNIT
     
+    local res = FU
     for i = 1, b do
         res = FixedMul(res, a)
     end
@@ -37,12 +42,26 @@ local function FixedPow(a, b)
 end
 
 local convertCharacterSelection = ZE2.convertCharacterSelection
+local darkbgheight = 60
+local darkbgheight2 = 3
+local darkbgtrans = 5
+local darkbgcolor = 31
+local darkbgshift = (darkbgtrans<<V_ALPHASHIFT)
+local skinlist
 
 return "CharacterSelect", function(v, player)
-    local selection = ZE2.charsel_selection
-    local prevselection = ZE2.charsel_prevselection
-    local anim = ZE2.charsel_anim
-    local setanim = ZE2.charsel_setanim
+	local selection = ZE2.charsel_selection
+	local prevselection = ZE2.charsel_prevselection
+	local anim = ZE2.charsel_anim
+	local setanim = ZE2.charsel_setanim
+
+	if drawScaled == nil then drawScaled = v.drawScaled end
+	if drawString == nil then drawString = v.drawString end
+	if drawFill == nil then drawFill = v.drawFill end
+	if drawLevelTitle == nil then drawLevelTitle = v.drawLevelTitle end
+	if getColormap == nil then getColormap = v.getColormap end
+	if getSprite2Patch == nil then getSprite2Patch = v.getSprite2Patch end
+	if cachePatch == nil then cachePatch = v.cachePatch end
 
 	if not (player.mo and player.mo.valid) then
 		return end;
@@ -56,26 +75,20 @@ return "CharacterSelect", function(v, player)
 
     if (ZE2.round_active) then
         return end;
-	
-	local darkbgheight = 60
-	local darkbgtrans = 5
-	local darkbgshift = (darkbgtrans<<V_ALPHASHIFT)
-	local darkbgcolor = 31
 
-	v.drawFill(0, 0, 600, darkbgheight, darkbgcolor+(darkbgshift|V_SNAPTOLEFT|V_SNAPTOTOP))
 	
+	drawFill(0, 0, 600, darkbgheight, darkbgcolor+(darkbgshift|V_SNAPTOLEFT|V_SNAPTOTOP))
 	for i=1,4 do
-		local darkbgheight2 = 3
 		local darkbgtrans2 = ((darkbgtrans+i)<<V_ALPHASHIFT)
-		v.drawFill(0, 60+((i-1)*darkbgheight2), 600, darkbgheight2, (darkbgcolor)+(darkbgtrans2|V_SNAPTOLEFT|V_SNAPTOTOP))
+		drawFill(0, 60+((i-1)*darkbgheight2), 600, darkbgheight2, (darkbgcolor)+(darkbgtrans2|V_SNAPTOLEFT|V_SNAPTOTOP))
 	end
 	
-	v.drawLevelTitle(10, 25, "Select A Character", V_SNAPTOTOP)
 	
+	drawLevelTitle(10, 25, "Select A Character", V_SNAPTOTOP)
 	local x = 160*FU
 	local y = 100*FU
 	
-    local skinlist = ZE2.getSkinNums(player)
+    if skinlist != ZE2.getSkinNums(player) then skinlist = ZE2.getSkinNums(player) end
 
     if not #skinlist then
         return end;
@@ -110,7 +123,7 @@ return "CharacterSelect", function(v, player)
 		if not (xxc > -60*FU and xxc < 400*FU) then -- remove offscreen
 			continue end;
 			
-		local iconpatch = v.getSprite2Patch(realskin, SPR2_LIFE, false, A)
+		local iconpatch = getSprite2Patch(realskin, SPR2_LIFE, false, A)
 		local iconscale = FixedMul((FU*6)/4, realskindata.highresscale)
 		local translation
 
@@ -118,10 +131,10 @@ return "CharacterSelect", function(v, player)
 			translation = "Grayscale"
 		end
 
-		local colormap = v.getColormap(realskin, realskindata.prefcolor, translation)
 		
-		v.drawScaled(xxc, yyc, iconscale, iconpatch, V_SNAPTOTOP, colormap)
-		v.drawString(xxc + 6*FU, yyc - 20*FU, cslot.max - cslot.count, V_SNAPTOTOP, "fixed")
+		local colormap = getColormap(realskin, realskindata.prefcolor, translation)
+		drawScaled(xxc, yyc, iconscale, iconpatch, V_SNAPTOTOP, colormap)
+		drawString(xxc + 6*FU, yyc - 20*FU, cslot.max - cslot.count, V_SNAPTOTOP, "fixed")
 	end
 	
 	-- Selected character code.
@@ -138,5 +151,5 @@ return "CharacterSelect", function(v, player)
 	CURWEAP_X = $ - FixedMul(CURWEAP.width*FU, CURWEAP_SCALE)/2
 	CURWEAP_Y = $ - FixedMul(CURWEAP.height*FU, CURWEAP_SCALE)/2
 	
-	v.drawScaled(CURWEAP_X, CURWEAP_Y - 6*FU, CURWEAP_SCALE, CURWEAP, V_SNAPTOTOP)
+	drawScaled(CURWEAP_X, CURWEAP_Y - 6*FU, CURWEAP_SCALE, CURWEAP, V_SNAPTOTOP)
 end, "game"
