4863ab479c5a2ca354ee883615409408cf019629
Jisk <danielthesmartypants2017@gmail.com>
2026-02-09 15:52:49 -0600

    refactor player_t metatable

--- src/Lua/xSlinger/metatables/player_t.lua
+++ src/Lua/xSlinger/metatables/player_t.lua
@@ -3,12 +3,36 @@ local old_index = mt.__index
 
 local xS_players = {}
 
+local function initPlayerNum(playernum, noinit, noteam)
+	if not noinit then
+		xS_players[playernum] = xSlinger.init()
+	end
+	
+	if players[playernum] then
+		xS_players[playernum].player = players[playernum]
+		
+		if players[playernum].mo then
+			xS_players[playernum].mo = players[playernum].mo
+		end
+	end
+	
+	if not noteam then
+		xS_players[playernum].team = 1
+	end
+	
+	return xS_players[playernum]
+end
+
 addHook("NetVars", function(net)
 	xS_players = net($)
 end)
 
+addHook("PlayerJoin", function(playernum)
+	initPlayerNum(playernum, false, false)
+end)
+
 addHook("PlayerQuit", function(player)
-	xS_players[player] = nil
+	xS_players[#player] = nil
 end)
 
 addHook("GameQuit", function()
@@ -17,19 +41,12 @@ end)
 
 mt.__index = function(player, key)
 	if key == "xSlinger" then
-		if not xS_players[player] then
-			xS_players[player] = xSlinger.init()
-			xS_players[player].player = player
-			xS_players[player].mo = player.mo
-			xS_players[player].team = 1
-			
-			return xS_players[player]
+		if not xS_players[#player] then
+			return initPlayerNum(#player, false, false)
 		else
 			-- Update references just in case.
-			xS_players[player].player = player
-			xS_players[player].mo = player.mo
 			
-			return xS_players[player]
+			return initPlayerNum(#player, true, true)
 		end
 	else
 		return old_index(player, key)
