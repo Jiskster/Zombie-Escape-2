7df56140b7b3ebd074701b3ffa588b6bc972fbae
Jisk <37682565+Jiskster@users.noreply.github.com>
2026-02-19 03:00:54 -0600

    dont access player + add fuse

--- src/Lua/xSlinger/invthink.lua
+++ src/Lua/xSlinger/invthink.lua
@@ -118,11 +118,11 @@ function xSlinger.DoThinker(mobj)
 		local tpViewMobj = setorigin and P_SetOrigin or P_MoveOrigin
 		
 		if holdobject then
-			local radius = player.realmo.radius
+			local radius = mobj.radius
 			local magnitude = (radius*3)/2
 			
-			local a = player.realmo.angle
-			local h = player.realmo.height/8
+			local a = mobj.angle
+			local h = mobj.height/8
 			
 			local x = holdobject.pos.x
 			local y = holdobject.pos.y
@@ -157,16 +157,18 @@ function xSlinger.DoThinker(mobj)
 			local zorigin = h + pz
 			
 			tpViewMobj(xS.viewmobj, 
-						player.mo.x + player.mo.momx + xorigin, 
-						player.mo.y + player.mo.momy + yorigin, 
-						player.mo.z + player.mo.momz + zorigin)
+						mobj.x + mobj.momx + xorigin, 
+						mobj.y + mobj.momy + yorigin, 
+						mobj.z + mobj.momz + zorigin)
+						
+			xS.viewmobj.fuse = 2
 		elseif (not holdobject) and (xS.viewmobj and xS.viewmobj.valid) then
 			P_RemoveMobj(xS.viewmobj)
 		end
 
 		if (xS.viewmobj and xS.viewmobj.valid) then
 			local iteminfo = xS:slot_get(xS.slot)
-			local item_sprite = iteminfo:getIndex("icon", player.mo.skin) -- TODO: use hold_object
+			local item_sprite = iteminfo:getIndex("icon", mobj.skin) -- TODO: use hold_object
 			if item_sprite then
 				xS.viewmobj.sprite = SPR_THOK --item_sprite
 				xS.viewmobj.dontdrawforviewmobj = mobj
