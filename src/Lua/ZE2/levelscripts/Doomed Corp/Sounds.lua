2673506a3daf03212e8ac0095c0d33de715ec27d
Lugent <lucariopokemonaura@gmail.com>
2026-03-16 16:48:43 -0400

    Fix broken sounds in Doomed Corp

--- src/Lua/ZE2/levelscripts/Doomed Corp/Sounds.lua
+++ src/Lua/ZE2/levelscripts/Doomed Corp/Sounds.lua
@@ -1,11 +1,11 @@
-sfxinfo[sfx_electr] = {
-        singular = false,
-        priority = 64,
-        flags = SF_X2AWAYSOUND
-}
-
 freeslot("sfx_dcrp01", "sfx_hell", "sfx_hell2", "sfx_electr")
 sfxinfo[sfx_electr].caption = "Electric Failure"
 sfxinfo[sfx_dcrp01].caption = "(RADIO) Soldier Dying"
 sfxinfo[sfx_hell].caption = "\"Your Hell awaits\""
 sfxinfo[sfx_hell2].caption = "\"What You Hope To Accomplish\"" -- What Do You Hope To Accomplish By Coming Here
+
+sfxinfo[sfx_electr] = {
+        singular = false,
+        priority = 64,
+        flags = SF_X2AWAYSOUND
+}
