9277e8b47d6f5db6ab3fcc64b7195b9b359b3b0f
Jisk <danielthesmartypants2017@gmail.com>
2026-04-28 19:36:11 -0500

    use custom player_t data instead of local tables

--- src/Lua/xSlinger/metatables/player_t.lua
+++ src/Lua/xSlinger/metatables/player_t.lua
@@ -1,52 +1,30 @@
 local mt = userdataMetatable("player_t")
 local old_index = mt.__index
 
-local xS_players = {}
-
-local function initPlayerNum(playernum, noinit, noteam)
-	if not noinit then
-		xS_players[playernum] = xSlinger.init()
-	end
-	
-	if players[playernum] then
-		xS_players[playernum].player = players[playernum]
-		
-		if players[playernum].mo then
-			xS_players[playernum].mo = players[playernum].mo
-		end
-	end
-	
-	if not noteam then
-		xS_players[playernum].team = 1
-	end
-	
-	return xS_players[playernum]
-end
-
-addHook("NetVars", function(net)
-	xS_players = net($)
-end)
-
-addHook("PlayerJoin", function(playernum)
-	initPlayerNum(playernum, false, false)
-end)
-
-addHook("PlayerQuit", function(player)
-	xS_players[#player] = nil
-end)
-
-addHook("GameQuit", function()
-	xS_players = {}
-end)
+local itername = "xSlinger[]"
 
 mt.__index = function(player, key)
 	if key == "xSlinger" then
-		if not xS_players[#player] then
-			return initPlayerNum(#player, false, false)
-		else
-			-- Update references just in case.
+		local internal_table = old_index(player, itername)
+		
+		if not internal_table then
+			player[itername] = xSlinger.init()
 			
-			return initPlayerNum(#player, true, true)
+			player[itername].player = player
+			
+			if player.mo then
+				player[itername].mo = player.mo
+			end
+			
+			return player[itername]
+		else
+			player[itername].player = player
+			
+			if player.mo then
+				player[itername].mo = player.mo
+			end
+			
+			return player[itername]
 		end
 	else
 		return old_index(player, key)
