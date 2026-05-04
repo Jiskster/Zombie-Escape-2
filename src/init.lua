72e405bc7b11aa2345a55b33d6d1beee104eebe5
GLide KS <84994993+GLideKS@users.noreply.github.com>
2026-05-04 16:10:32 -0400

    Fix crates not liking FOFs

--- src/init.lua
+++ src/init.lua
@@ -1,7 +1,7 @@
 local dofile = dofile;
 
 freeslot("MT_ZVISUAL") -- just put it here, is gonna be a general propuse mobj visual
-mobjinfo[MT_ZVISUAL] = {doomednum = -1, spawnstate = S_INVISIBLE, speed = 0, radius = 0, height = 0, flags = MF_NOBLOCKMAP|MF_NOGRAVITY}
+mobjinfo[MT_ZVISUAL] = {doomednum = -1, spawnstate = S_INVISIBLE, speed = 0, radius = 0, height = 0, flags = MF_NOBLOCKMAP|MF_NOGRAVITY|MF_NOCLIPHEIGHT}
 
 dofile "xSlinger/init" -- Load xSlinger
 
