0c4e4ce479418118e5fd57f96c76a1edeb20d97f
GLide KS <84994993+GLideKS@users.noreply.github.com>
2026-05-05 00:58:43 -0400

    For some reason UZB doesn't like this

--- src/Lua/ZE2/libraries/worldtext.lua
+++ src/Lua/ZE2/libraries/worldtext.lua
@@ -1,5 +1,12 @@
 freeslot("MT_ZTEXT")
-mobjinfo[MT_ZTEXT] = {doomednum = -1, spawnstate = S_INVISIBLE, speed = 0, radius = 0, height = 0, flags = MF_NOBLOCKMAP|MF_NOGRAVITY|MF_NOCLIPHEIGHT|MF_NOCLIPTHING|MF_NOCLIP}
+mobjinfo[MT_ZTEXT] = {
+	doomednum = -1,
+	spawnstate = S_INVISIBLE,
+	speed = 0,
+	radius = 0,
+	height = 0,
+	flags = MF_NOBLOCKMAP|MF_NOGRAVITY|MF_NOCLIPHEIGHT|MF_NOCLIPTHING|MF_NOCLIP
+}
 
 freeslot("SPR_LOWERCASE_CHARACTERS", "SPR_NUMBER_CHARACTERS", "SPR_SYMBOLS_CHARACTERS", "SPR_UPPERCASE_CHARACTERS")
 
