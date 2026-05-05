2bf66b7425e84c5471cbab319c60e74b71a56fd4
GLide KS <84994993+GLideKS@users.noreply.github.com>
2026-05-05 01:48:17 -0400

    [Westopolis] Do not rely on delayed linedef executors for timed barriers
    I guess ByPigs wasn't aware of this

--- src/Lua/ZE2/levelscripts/Westopolis/Timers.lua
+++ src/Lua/ZE2/levelscripts/Westopolis/Timers.lua
@@ -1,7 +1,7 @@
 ZE2:AddTimer("WESTOPOLIS_1", {
 	text = "Defend Spring Container",
 	time = 30*TICRATE,
-	on_end_tag = 5,
+	on_end_tag = 98,
 	textcolor = SKINCOLOR_PINK,
 	lua_linedef_exec = "WESTOP1",
 })
@@ -9,7 +9,7 @@ ZE2:AddTimer("WESTOPOLIS_1", {
 ZE2:AddTimer("WESTOPOLIS_2", {
 	text = "Defend Barrier",
 	time = 60*TICRATE,
-	on_end_tag = 17,
+	on_end_tag = 112,
 	textcolor = SKINCOLOR_GREY,
 	lua_linedef_exec = "WESTOP2",
 })
