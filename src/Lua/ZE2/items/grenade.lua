c1c17b5c162711da93b0050282e0dedd31bd5268
Jisk <danielthesmartypants2017@gmail.com>
2026-07-21 17:33:59 -0500

    make grenade ring usable (+revamp)

--- src/Lua/ZE2/items/grenade.lua
+++ src/Lua/ZE2/items/grenade.lua
@@ -1,4 +1,14 @@
+freeslot("S_XS_GRENADERING")
 freeslot("S_XS_GRENADERING_DROP")
+
+states[S_XS_GRENADERING] = {
+	sprite = SPR_TGRE,
+	frame = FF_ANIMATE|FF_FULLBRIGHT,
+	tics = -1,
+	var1 = 17,
+	var2 = 1,
+	nextstate = S_XS_GRENADERING_DROP,
+}
 
 states[S_XS_GRENADERING_DROP] = {
 	sprite = SPR_RNGG,
@@ -9,15 +19,95 @@ states[S_XS_GRENADERING_DROP] = {
 	nextstate = S_XS_GRENADERING_DROP,
 }
 
+local missile_grenade_ring = 
+xSlinger.registerMissile("GRENADE_RING", {
+	speed = 25*FRACUNIT,
+	displayname = "Grenade Ring",
+	state = S_XS_GRENADERING,
+	deathstate = S_RINGEXPLODE,
+	deathsound = sfx_pop,
+	delflags = MF_NOGRAVITY,
+	safeground = true,
+	tick = function(pmo, mo)
+		local prevmomz = mo.grenade_prevmomz or 0
+		local floorhit = (mo.eflags & MFE_JUSTHITFLOOR) > 0
+		local ceilinghit = (mo.z + mo.height == P_CeilingzAtPos(mo.x, mo.y, mo.z, mo.height))
+		
+		if floorhit or ceilinghit then
+			local flip = P_MobjFlip(mo)
+			
+			if ceilinghit then flip = -$ end
+			
+			local newprevmomz = FixedDiv(abs(prevmomz), 6*FU/4)
+			
+			S_StartSound(mo, sfx_s3k5d)
+			P_SetObjectMomZ(mo, newprevmomz*flip, true)
+			
+			mo.momx = FixedDiv($, 6*FU/4)
+			mo.momy = FixedDiv($, 6*FU/4)
+			
+			if abs(mo.momz) < FRACUNIT then
+				mo.momx = 0; mo.momy = 0; mo.momz = 0
+			end
+		end
+		
+		local faststart = 15
+		
+		if mo.fuse > faststart then
+			if (mo.fuse % 12) == 0 then
+				S_StartSound(mo, sfx_gbeep)
+				
+				local ghost = P_SpawnGhostMobj(mo)
+				ghost.fuse = 8
+				ghost.color = SKINCOLOR_GREEN
+				ghost.colorized = true
+			end
+		else
+			if (mo.fuse % 3) == 0 then
+				S_StartSound(mo, sfx_gbeep)
+				S_StartSoundAtVolume(mo, sfx_deton, min(10 + (faststart - mo.fuse)*27, 255))
+				
+				local ghost = P_SpawnGhostMobj(mo)
+				ghost.fuse = 3
+				ghost.color = SKINCOLOR_RED
+				ghost.colorized = true
+			end
+		end
+		
+		mo.grenade_prevmomz = mo.momz
+	end,
+	blocked = function(pmo, mo, line)
+		if not line then
+			return end;
+		
+		local side = P_PointOnLineSide(mo.x, mo.y, line)
+		local angle = R_PointToAngle2(line.v1.x, line.v1.y, line.v2.x, line.v2.y)
+					  + (ANGLE_90 * (side and 1 or -1))
+		local speed = FixedHypot(mo.momx, mo.momy)
+		
+		P_InstaThrust(mo, angle, speed)
+		S_StartSound(mo, sfx_s3k5d)
+		
+		return true
+	end
+})
+
 xSlinger.registerItem("grenade_ring",  {
 	displayname = "Grenade";
 
-	missile = MT_THROWNGRENADE;
+	missile = "GRENADE_RING";
 
 	dropstate = S_XS_GRENADERING_DROP;
 
 	icon = "GRENIND";
 
+	sounds = {
+		use = sfx_s3k5d;
+		reload = {sfx_xsrel1, sfx_xsrel2};
+		pickup = sfx_None;
+		drop = sfx_None;
+	};
+	
 	firerate = 15;
 
 	color = SKINCOLOR_GREEN;
@@ -30,6 +120,4 @@ xSlinger.registerItem("grenade_ring",  {
 	maxcount = 10;
 
 	fuse = 2*TICRATE;
-
-	velocity_multiplier = FRACUNIT/2;
 })
