e24aba5790b4e97244b859ac2cb85f32a0a65723
Jisk <danielthesmartypants2017@gmail.com>
2026-07-21 00:37:54 -0500

    port explosion ring

--- src/Lua/ZE2/items/explosion_ring.lua
+++ src/Lua/ZE2/items/explosion_ring.lua
@@ -1,23 +1,39 @@
-freeslot("S_XS_EXPLOSIONRING_DROP")
+freeslot("S_XS_EXPLOSIONRING")
 
-states[S_XS_EXPLOSIONRING_DROP] = {
+states[S_XS_EXPLOSIONRING] = { -- for drop only
 	sprite = SPR_RNGE,
 	frame = FF_ANIMATE|FF_FULLBRIGHT,
 	tics = -1,
 	var1 = 34,
 	var2 = 1,
-	nextstate = S_XS_EXPLOSIONRING_DROP,
+	nextstate = S_XS_EXPLOSIONRING,
 }
+
+local missile_explosion_ring = xSlinger.registerMissile("EXPLOSION_RING", {
+	speed = 60*FRACUNIT,
+	displayname = "Explosion Ring",
+	state = S_THROWNEXPLOSION1,
+	deathstate = S_RINGEXPLODE,
+	deathsound = sfx_pop,
+	delflags = MF_NOGRAVITY,
+})
 
 xSlinger.registerItem("explosion_ring", {
 	displayname = "Explosion Ring";
 
-	missile = MT_THROWNEXPLOSION;
+	missile = "EXPLOSION_RING";
 
-	dropstate = S_XS_EXPLOSIONRING_DROP;
+	dropstate = S_XS_EXPLOSIONRING;
 
 	icon = "XSG_BOMB";
 
+	sounds = {
+		use = sfx_cannon;
+		reload = {sfx_xsrel1, sfx_xsrel2};
+		pickup = sfx_None;
+		drop = sfx_None;
+	};
+	
 	firerate = (TICRATE*3)/2;
 
 	ammo = 3;
@@ -30,8 +46,4 @@ xSlinger.registerItem("explosion_ring", {
 
 	knockback = 100*FRACUNIT;
 	knockback_tics = TICRATE;
-
-	missile_spawn = function(self, mobj, missile)
-		missile.flags = $ & ~(MF_NOGRAVITY)
-	end;
 })
