5a0bbc50d5c07e6211330507fa095a9ae209bbd7
Jisk <danielthesmartypants2017@gmail.com>
2026-07-21 00:06:58 -0500

    fix scatter

--- src/Lua/ZE2/items/scatter_ring.lua
+++ src/Lua/ZE2/items/scatter_ring.lua
@@ -30,7 +30,7 @@ local missile_scatter_ring = xSlinger.registerMissile("SCATTER_RING", {
 	state = S_XS_SCATTERRING,
 	deathstate = S_SPRK1,
 	deathsound = sfx_itemup,
-	height = 32,
+	height = 32*FRACUNIT,
 }) -- height 32
 
 xSlinger.registerItem("scatter_ring", {
