2687440ed47f013c62b592c21308f2a72b12422f
Jisk <danielthesmartypants2017@gmail.com>
2026-07-23 04:08:30 -0500

    fix hitbox definition fuckups on bounce ring

--- src/Lua/ZE2/items/bounce_ring.lua
+++ src/Lua/ZE2/items/bounce_ring.lua
@@ -48,8 +48,8 @@ xSlinger.registerMissile("BOUNCE_RING", {
 	deathsound = sfx_itemup,
 	nogravitydeath = true,
 	safeground = true,
-	width = 32*FRACUNIT,
-	height = 32*FRACUNIT,
+	radius = 48*FRACUNIT,
+	height = 48*FRACUNIT,
 	tick = bounce_tick,
 	subtick = bounce_tick,
 	blocked = function(pmo, mo, line)
