15dd16dee08e0acce09aec98fb21f89cebbd0036
Jisk <danielthesmartypants2017@gmail.com>
2026-07-24 23:21:24 -0500

    dont draw hp when player is spectator

--- src/Lua/ZE2/hud/drawers/Info.lua
+++ src/Lua/ZE2/hud/drawers/Info.lua
@@ -118,6 +118,8 @@ local function health(v,p,me,ze)
 	local team = p.xSlinger.team
 
 	if (health == nil) or (maxhealth == nil) then ResetInfos(); return end
+	if p.spectator then return end
+	
 	--if ZE2.pregame_timeleft then ResetInfos(); return end
 	maxhealth = $*FU
 	AlreadyReset = false
