44a9481087f00148fc8bc6dee3a501ceace1708d
Lugent <lucariopokemonaura@gmail.com>
2026-07-24 22:47:56 -0400

    Make shield HUD display numbers instead of percentage

--- src/Lua/ZE2/hud/drawers/Info.lua
+++ src/Lua/ZE2/hud/drawers/Info.lua
@@ -391,7 +391,7 @@ local function health(v,p,me,ze)
 		)
 
 		v.drawString(x,y - (FU * 7/2),
-			string.format("%.0f%%", percentage*100),
+			me.shield_health,
 			flags,
 			"thin-fixed-center"
 		)
