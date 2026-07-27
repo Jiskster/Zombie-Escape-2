2b4203af32a0418189dfa46a1bc1fa040d49360c
Jisk <danielthesmartypants2017@gmail.com>
2026-07-26 22:29:16 -0500

    add more debug prints

--- src/Lua/xSlinger/missile.lua
+++ src/Lua/xSlinger/missile.lua
@@ -434,8 +434,13 @@ addHook("NetVars", function(net)
 			continue
 		end
 		
-		if mobj.iteminfo then
-			setmetatable(mobj.iteminfo, xSlinger.METATABLES.ITEMINFO)
+		if not mobj.iteminfo then
+			print("no iteminfo")
+		else
+			print("has iteminfo")
+			if not getmetatable(mobj.iteminfo) then
+				print("no metatable for iteminfo")
+			end
 		end
 	end
 end)
