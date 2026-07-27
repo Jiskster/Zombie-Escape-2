e907774e9bb6f08a675aee16250c1844b93872f2
Jisk <danielthesmartypants2017@gmail.com>
2026-07-27 02:12:47 -0500

    make alphazombie.rage_regen a bit more readable

--- src/Lua/ZE2/gametype.lua
+++ src/Lua/ZE2/gametype.lua
@@ -59,10 +59,17 @@ xSlinger.registerEffect("alphazombie.rage_regen", {
 	---@param time_left tic_t
 	tick = function(effect, mobj, time_left)
 		local player = mobj.player
-		if not player or not player.valid then return end
 
-		if (player.ze2.special_cooldown <= 0) then return end
+		if not (player and player.valid) then 
+			return
+		end
+
+		if (player.ze2.special_cooldown <= 0) then 
+			return 
+		end
+
 		player.ze2.special_cooldown = player.ze2.special_cooldown - 2
+
 		if (player.ze2.special_cooldown < 0) then
 			player.ze2.special_cooldown = 0
 		end
