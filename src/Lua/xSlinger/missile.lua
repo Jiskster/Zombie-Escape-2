c4f8c237f97da056689ec40b7a81c888ed4c1893
Jisk <danielthesmartypants2017@gmail.com>
2026-07-26 22:21:13 -0500

    add debug prints

--- src/Lua/xSlinger/missile.lua
+++ src/Lua/xSlinger/missile.lua
@@ -113,6 +113,10 @@ function xSlinger.SpawnMissile(m_table)
 		end
 
 		th.iteminfo = temp_iteminfo
+		
+		if not (getmetatable(th.iteminfo)) then
+			print("Jisk you were right! Theres not metatable found!!!")
+		end
 
 		th.velprec = missile_velocity_precision
 
@@ -133,6 +137,8 @@ function xSlinger.SpawnMissile(m_table)
 		if sounds.use ~= nil then
 			firesound = sounds.use
 		end
+	else
+		print("huh??")
 	end
 
 	th.team = source.team
