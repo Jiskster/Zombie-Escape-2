98bf86fb0e4c7dc6866891c91a74f84f7ccddb84
Jisk <danielthesmartypants2017@gmail.com>
2026-07-27 15:51:00 -0500

    [Database Z] Finally rename code to Database Z

--- src/Lua/ZE2/levelscripts/Database Z/Timers.lua
+++ src/Lua/ZE2/levelscripts/Database Z/Timers.lua
@@ -1,23 +1,23 @@
-ZE2:AddTimer("NOXY_1", {
+ZE2:AddTimer("DATAZ_1", {
 	text = "Defend Barrier",
 	time = 25*TICRATE,
 	on_end_tag = 2,
 	textcolor = SKINCOLOR_YELLOW,
-	lua_linedef_exec = "NOXYTIMER1",
+	lua_linedef_exec = "DATAZTIMER1",
 })
 
-ZE2:AddTimer("NOXY_2", {
+ZE2:AddTimer("DATAZ_2", {
 	text = "Defend Final Barrier",
 	time = 40*TICRATE,
 	on_end_tag = 6,
 	textcolor = SKINCOLOR_PURPLE,
-	lua_linedef_exec = "NOXYTIMER2",
+	lua_linedef_exec = "DATAZTIMER2",
 })
 
-ZE2:AddTimer("NOXY_3", {
+ZE2:AddTimer("DATAZ_3", {
 	text = "Leaving Platform",
 	time = 20*TICRATE,
 	on_end_tag = 23,
 	textcolor = SKINCOLOR_RED,
-	lua_linedef_exec = "NOXYTIMER3",
+	lua_linedef_exec = "DATAZTIMER3",
 })
