c4675b458bc05f1a81ae3df91872a3875cac53e4
GLide KS <84994993+GLideKS@users.noreply.github.com>
2026-07-30 20:17:26 -0400

    Extend a bit more the infection VFX
    up to 1.5 seconds.

--- src/Lua/ZE2/main/zombie/infection_fx.lua
+++ src/Lua/ZE2/main/zombie/infection_fx.lua
@@ -1,17 +1,19 @@
+local fx_time = TICRATE * 3 / 2 -- Should be paired up with the timer on ZombifyPlayer.lua
+local spritescale = FU
+
 addHook("MobjThinker", function(mo)
-    if (leveltime % 5 == 0) then return end
+    if (leveltime % 3 == 0) then return end
     if not (mo and mo.valid) then return end
     if not mo.infectionfx then return end
 
     local x = P_RandomRange(- mo.radius / FU, mo.radius / FU)
     local y = P_RandomRange(- mo.radius / FU, mo.radius / FU)
     local z = P_RandomRange(0, (mo.height / FU) / 3 * 2)
-    local fx_time = TICRATE / 2 -- Should be paired up with the timer on ZombifyPlayer.lua
     local ratio = FixedDiv(mo.infectionfx * FU, fx_time * FU)
 
     local infectparticle = P_SpawnMobjFromMobj(mo, x * FU, y * FU, z * FU, MT_THOK)
-    infectparticle.spritexscale = FixedMul(ratio, FU)
-    infectparticle.spriteyscale = FixedMul(ratio, FU)
+    infectparticle.spritexscale = FixedMul(ratio, spritescale)
+    infectparticle.spriteyscale = FixedMul(ratio, spritescale)
     infectparticle.state = S_SPINBOBERT_FIRE_TRAIL1
     infectparticle.colorized = true
     infectparticle.color = mo.color
