65ef3909ff253cf355695605f98255b144fb9ef2
GLide KS <84994993+GLideKS@users.noreply.github.com>
2026-07-30 20:55:38 -0400

    Optimize blood droplets by 33%

--- src/Lua/ZE2/main/zombie/blood_droplets.lua
+++ src/Lua/ZE2/main/zombie/blood_droplets.lua
@@ -1,13 +1,22 @@
 -- Blood droplets will spawn when a zombie gets hit... or whatever use you want to do with this.
 
-local droplt_scale = FU * 4 / 9
+------- Main object -------
 
--- Main object
 freeslot(
     "MT_ZOMB_DROPLET",
     "S_ZDROPLET",
     "S_ZDROPLET_DEATH"
 )
+
+-- Anything for that 10% optimization is received lol
+local xS_addHook = xSlinger.addHook
+local MT_ZOMB_DROPLET = MT_ZOMB_DROPLET
+local S_ZDROPLET = S_ZDROPLET
+local S_ZDROPLET_DEATH = S_ZDROPLET_DEATH
+local MT_PLAYER = MT_PLAYER
+-- Droplet properties
+local droplt_scale = FU * 4 / 9
+local translation = "goop_to_blood"
 
 mobjinfo[MT_ZOMB_DROPLET] = {
     doomednum = -1,
@@ -21,7 +30,7 @@ mobjinfo[MT_ZOMB_DROPLET] = {
 states[S_ZDROPLET] = {SPR_GOOP, A|FF_TRANS30, -1, function(mo) -- A good oportunity to set up the blood droplet here
     mo.spritexscale = droplt_scale
     mo.spriteyscale = droplt_scale
-    mo.translation = "goop_to_blood"
+    mo.translation = translation
     P_SetObjectMomZ(mo, FU * 3)
 end, nil, nil, S_ZDROPLET}
 
@@ -32,25 +41,24 @@ states[S_ZDROPLET_DEATH] = {SPR_GOOP, C|FF_TRANS30, 2 * TICRATE, function(mo)
     mo.momy = 0
 end, 0, 0, S_NULL}
 
-addHook("MobjThinker", function(mo) -- MobjHitFloor my beloved where are you?! TODO: Replace this with MobjHitFloor in v2.2.16
-    if not (mo and mo.valid) then return end
-    if mo.state == mo.info.deathstate then return end
-    if mo.state != S_ZDROPLET then return end
+------- Droplet behavior -------
+
+local function zombie_droplet_hitground(mo) -- MobjHitFloor my beloved where are you?! TODO: Replace this with MobjHitFloor in v2.2.16
+    if not mo.valid then return end
     if not P_IsObjectOnGround(mo) then return end
 
-    mo.state = mo.info.deathstate
-end, MT_ZOMB_DROPLET)
+    if mo.state != S_ZDROPLET_DEATH then
+        mo.state = S_ZDROPLET_DEATH
+    end
+end
 
-local function Z_DoDroplet(p, blood_quantity)
-    local mo = p.mo
-
+local function Z_DoDroplet(mo, blood_quantity)
+    local pradius = mo.radius / FU -- Idk, P_RandomRange throws a warning about big numbers so I had to divide by FU first
+    local pheight = mo.height / FU
 
     for i = 1, blood_quantity do
-        local pradius = mo.radius / FU -- Idk, P_RandomRange throws a warning about big numbers so I had to divide by FU first
-        local pheight = mo.height / FU
         local spawnpos_xy = P_RandomRange(0, pradius / 2) * FU
         local spawnpos_z = P_RandomRange(pheight / 4, pheight / 3 * 2) * FU
-
         local drop_rad = FixedAngle(P_RandomRange(0, 360))
         local droplt = P_SpawnMobjFromMobj(mo, spawnpos_xy, spawnpos_xy, spawnpos_z, MT_ZOMB_DROPLET)
 
@@ -58,18 +66,24 @@ local function Z_DoDroplet(p, blood_quantity)
     end
 end
 
-xSlinger.addHook("OnPlayerDamage", function(p, inf, src, dmg, damagetype)
-    if not (p.mo and p.mo.valid and p.mo.team == 2) then return end
+local function zombie_dropblood(p, inf, src, dmg, damagetype)
+    if not (p.mo and p.mo.valid) then return end
+    if p.mo.team != 2 then return end
     if dmg <= 4 then return end -- Yeah no I'll not let you spam it lol
+    local pmo = p.mo
 
     local dmg_strenght = (dmg * 10) / 100
     local blood_count = max(2, min(dmg_strenght, 30))
 
-    Z_DoDroplet(p, blood_count)
-end)
+    Z_DoDroplet(pmo, blood_count)
+end
 
-addHook("MobjDeath", function(mo) -- Drop blood also on death duh
-    if not (mo and mo.valid and mo.team == 2) then return end
+local function zombie_death(mo) -- Drop blood also on death duh
+    if mo.team != 2 then return end
+    Z_DoDroplet(mo, 20)
+end
 
-    Z_DoDroplet(mo.player, 20)
-end, MT_PLAYER)
+-- Now we hook everything
+xS_addHook("OnPlayerDamage", zombie_dropblood)
+addHook("MobjThinker", zombie_droplet_hitground, MT_ZOMB_DROPLET)
+addHook("MobjDeath", zombie_death, MT_PLAYER)
