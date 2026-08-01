04cafea3ac1c621302eca65e3e30b83b141474aa
Lugent <lucariopokemonaura@gmail.com>
2026-08-01 13:04:20 -0400

    Fix HUD moment giving desyncs

--- src/Lua/ZE2/hud/setup.lua
+++ src/Lua/ZE2/hud/setup.lua
@@ -13,6 +13,10 @@ local function SetupHud(filename)
 	local new_drawfunc = function(v, player)
 		if (gametype ~= GT_ZE2) then return end
 
+		if player and player.valid and not player["xSlinger[]"] then
+			return
+		end
+
 		drawfunc(v, player)
 	end
 
