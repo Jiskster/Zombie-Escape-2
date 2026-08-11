ccd5166dabaafe6cdd499c27e7c52fa05d5058ff
Lugent <lucariopokemonaura@gmail.com>
2026-08-10 23:37:18 -0400

    Fix missing check in playertags

--- src/Lua/ZE2/hud/drawers/PlayerTags.lua
+++ src/Lua/ZE2/hud/drawers/PlayerTags.lua
@@ -1,26 +1,24 @@
 local function getPlayersByDist(targetplr)
 	local output = {}
 	local dists = {}
-	
 	for player in players.iterate do
 		if not (player.mo and player.mo.valid) then
 			continue end;
 
 		if (player == targetplr) then
 			continue end;
-			
+
 		output[#output + 1] = player
 	end
-	
-	for i=1, #output do
+
+	for i = 1, #output do
 		local player = output[i]
 		dists[output[i]] = R_PointToDist2(targetplr.mo.x, targetplr.mo.y, player.mo.x, player.mo.y)
 	end
-	
+
 	table.sort(output, function(a,b)
 		return dists[a] > dists[b]
 	end)
-	
 	return output
 end
 
@@ -30,13 +28,12 @@ return "PlayerTags", function(v, player)
 
 	if (ZE2.game_ended) then
 		return end;
-	
-	local orderedPlayers = getPlayersByDist(player)
 
+	local orderedPlayers = getPlayersByDist(player)
 	local pointpatch = v.cachePatch("Z_POINTTEAM")
 	for i=1, #orderedPlayers do
 		local plr = orderedPlayers[i]
-		
+
 		if (plr.mo.team ~= player.mo.team) then
 			continue end;
 
@@ -66,8 +63,8 @@ return "PlayerTags", function(v, player)
 			offset1 = $/2
 			offset2 = $/2
 		end
-		
-		if plr.mo.health <= (plr.mo.maxhealth/2) then
+
+		if (plr.mo.maxhealth ~= nil) and (plr.mo.health <= (plr.mo.maxhealth / 2)) then
 			healthmap = V_REDMAP
 		end
 
