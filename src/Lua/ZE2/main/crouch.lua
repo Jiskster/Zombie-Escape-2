365c9e7cc764d401b93f96f3d8e383fd1d82d675
Jisk <37682565+Jiskster@users.noreply.github.com>
2026-08-12 00:58:34 -0500

    Fix first person crouching in singleplayer

--- src/Lua/ZE2/main/crouch.lua
+++ src/Lua/ZE2/main/crouch.lua
@@ -146,7 +146,7 @@ addHook("PostThinkFrame", function()
 	for player in players.iterate do
 		if not (player.mo and player.mo.valid) then continue end
 		if ZE2.game_ended then continue end
-		if ZE2.pregame_timeleft then continue end
+		if ZE2.pregame_timeleft and multiplayer then continue end
 
 		if player.ze2.crouching and switchablestates[player.mo.state] then
 			if not (player.mo.state == S_PLAY_CROUCH_ZE2) then
