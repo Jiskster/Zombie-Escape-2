d268f14768760027be9bf7f9ca02ab7e0e86c4df
Jisk <37682565+Jiskster@users.noreply.github.com>
2026-09-09 16:29:26 -0500

    Revert "ring boom vfx: use P_SetMobjStateNF & change vfx"
    This reverts commit 75294e0abe56e5cf0cd9f518677413664c26d5ff.

--- src/Lua/game/Core/Items/init.lua
+++ src/Lua/game/Core/Items/init.lua
@@ -4,10 +4,9 @@ freeslot("S_ZE2_RINGEXPLODE")
 -- TODO: Remake items that use this and remove this action override.
 function A_RingExplode2(actor, var1, var2)
     local vfx = P_SpawnMobj(actor.x, actor.y, actor.z, MT_THOK)
-	P_SetMobjStateNF(vfx, S_TNTBARREL_EXPL1)
-	vfx.tics = 2
+    vfx._override_tnt_explode = true
+    vfx.state = S_TNTBARREL_EXPL1
     vfx.fuse = TICRATE
-	vfx.team = actor.team
     actor.state = S_INVISIBLE
 	actor.fuse = TICRATE
 	actor.flags = $ | MF_NOGRAVITY
@@ -27,6 +26,12 @@ function A_RingExplode2(actor, var1, var2)
         actor.flags2 = actor.flags2 | MF2_DEBRIS
         P_DamageMobj(foundmobj, actor, actor.target, 1, 0)
     end, actor, actor.x - bm_range, actor.x + bm_range, actor.y - bm_range, actor.y + bm_range)
+end
+
+function A_TNTExplode(actor, var1, var2)
+    if not actor._override_tnt_explode then
+        super(actor, var1, var2)
+    end
 end
 
 states[S_ZE2_RINGEXPLODE] = {SPR_NULL, A, 1, A_RingExplode2, 0, 0, S_XPLD1, 0}
