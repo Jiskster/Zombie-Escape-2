a413f1079ab5c00dee1406594aba59a599ee07ae
Jisk <37682565+Jiskster@users.noreply.github.com>
2026-09-21 17:46:12 -0500

    fix funny bug with accel ring

--- src/Lua/game/Core/Items/Survivor/accel_ring.lua
+++ src/Lua/game/Core/Items/Survivor/accel_ring.lua
@@ -34,7 +34,7 @@ xSlinger.registerMissile("ACCEL_RING", {
 	state = S_ZE2_THROWNACCEL,
 	deathstate = S_SPRK1,
 	deathsound = sfx_rs_di2,
-	tick = function(pmo, mo)
+	tick = function(self, pmo, mo)
 		mo.momx = $ * 12/11
 		mo.momy = $ * 12/11
 		mo.momz = $ * 12/11
